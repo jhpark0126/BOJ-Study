828404befb64be1509a818f5c52a39d6d340a3f1
jhpark0126 <37992062+jhpark0126@users.noreply.github.com>
2018-09-02 15:53:41 +0900

    1로만들기

--- 1463.cpp
+++ 1463.cpp
@@ -29,13 +29,26 @@ int DP(int n)
 	return d[n];
 }
 
+int DP_botup(int n)
+{
+	d[1] = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0 && d[i] > d[i / 2] + 1)
+			d[i] = d[i / 2] + 1;
+		if (i % 3 == 0 && d[i] > d[i / 3] + 1)
+			d[i] = d[i / 3] + 1;
+	}
+	return d[n];
+}
 int main(void)
 {
 	int N;
 	cin >> N;
 
 	cout << DP(N) << endl;
-
+	//cout << DP_botup(N) << endl;
 
 	return 0;
 }
